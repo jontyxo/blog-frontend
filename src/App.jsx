87441966b1b1d0a6deb932f9b92f60e670d0a301
Joyesh Wadhwa <65168230+jontyxo@users.noreply.github.com>
2023-02-24 19:32:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,82 @@ import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createBrowserRouter,createRoutesFromElements,Route,Link,Outlet,RouterProvider } from "react-router-dom";
 import { useContext } from "react";
-import { Context } from "./context/Context";
+import { Context, ContextProvider } from "./context/Context";
+
 import Contact from "./pages/contact/contact";
 
 function App() {
   const {user}=useContext(Context)
+  
+  const router=createBrowserRouter(
+    
+    createRoutesFromElements(
+      
+      <Route path="/" element={<Root />}>
+        <Route index element={<Homepage />} />
+        <Route path="/contact"
+          element= {<Contact />} />
+          <Route path="/register" element=
+           {user ? <Homepage /> : <Register />} />
+         <Route path="/login" element={user ? <Homepage /> : <Login />} />
+         <Route path="/post/:id" element=
+           {<Single />} />
+         
+        
+      
+          <Route path="/write" element={user ? <Write /> : <Login />} />
+         <Route path="/settings" element=
+           {user ? <Settings /> : <Login />} />
+      
+
+      </Route>
+    )
+  )
+
   return (
-    <Router>
-      <Topbar />
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route path="/posts">
-          <Homepage />
-        </Route>
-        <Route path="/register">
-          {user ? <Homepage /> : <Register />}
-        </Route>
-        <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
-        <Route path="/post/:id">
-          <Single />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/write">{user ? <Write /> : <Login />}</Route>
-        <Route path="/settings">
-          {user ? <Settings /> : <Login />}
-        </Route>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Topbar />
+    //   <Switch>
+    //     <Route exact path="/">
+    //       <Homepage />
+    //     </Route>
+    //     <Route path="/posts">
+    //       <Homepage />
+    //     </Route>
+    //     <Route path="/register">
+    //       {user ? <Homepage /> : <Register />}
+    //     </Route>
+    //     <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
+    //     <Route path="/post/:id">
+    //       <Single />
+    //     </Route>
+    //     <Route path="/contact">
+    //       <Contact />
+    //     </Route>
+    //     <Route path="/write">{user ? <Write /> : <Login />}</Route>
+    //     <Route path="/settings">
+    //       {user ? <Settings /> : <Login />}
+    //     </Route>
+    //   </Switch>
+    // </Router>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
+}
+
+const Root=()=>{
+
+  return (
+    <>
+        <Topbar />
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
 }
 
 export default App;
