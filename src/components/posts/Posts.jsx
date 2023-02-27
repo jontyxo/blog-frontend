bdf291c7ca77070039d67f7861f1d744a8daad0c
Joyesh Wadhwa <65168230+jontyxo@users.noreply.github.com>
2023-02-27 16:55:41 +0530

    Update Posts.jsx

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,8 +4,9 @@ import "./posts.css";
 export default function Posts({posts}) {
   return (
     <div className="posts">
-   code
-   
+   {posts.map((p)=>(
+      <Post key={p._id} post={p}/>
+    ))}
     
     </div>
   );
